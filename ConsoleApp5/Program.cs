37a75079f0aba198ee3447080cfbc027bc232a5a
narek <narek@DESKTOP-98JS365>
2024-06-19 18:19:26 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] strings = { "apple", "banana", "orange", "pear" };
+
+        Console.WriteLine("Введите строку для поиска:");
+        string userInput = Console.ReadLine();
+
+        bool found = false;
+        foreach (string str in strings)
+        {
+            if (str == userInput)
+            {
+                found = true;
+                break;
+            }
+        }
+
+        if (found)
+        {
+            Console.WriteLine("Строка найдена!");
+        }
+        else
+        {
+            Console.WriteLine("Строка не найдена!");
+        }
+    }
+}
+
